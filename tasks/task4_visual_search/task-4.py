7fd84856b61f48e586d8a42de31761b92bc8e271
47Nguyen <ankhanhthinj@gmail.com>
2026-09-05 19:16:15 +0700

    Task 4 - triplet loss image task4_visual_search

--- tasks/task4_visual_search/task-4.py
+++ tasks/task4_visual_search/task-4.py
@@ -1,6 +1,22 @@
-import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.pyplot as plt 
+import os
+import pandas as pd
+import random
+import tensorflow as tf
+from pathlib import Path
+from keras import applications
+from keras import layers
+from keras import losses
+from keras import metrics
+from keras import Model
+from keras.applications import resnet
+
+
+target_shape = (200, 200)
+# https://keras.io/examples/vision/siamese_network/
+
+
 
 train_path = './A2_FashionDataset/FashionDataset/train/styles_train.csv'
 test_path = './A2_FashionDataset/FashionDataset/test/styles_prediction.csv'
@@ -8,8 +24,23 @@ test_path = './A2_FashionDataset/FashionDataset/test/styles_prediction.csv'
 df_train = pd.read_csv(train_path)
 df_test = pd.read_csv(test_path)
 
-# print(df_train.head())
+# Setup image path for products
+df_train['path'] = './A2_FashionDataset/FashionDataset/train/images_train' + '/' + df_train['id'].astype(str) + '.jpg'
 
+# Preprocess dataset
+df_train = df_train.drop(columns=['Unnamed: 10','Unnamed: 11'])
 
-print(df_test.head())
+# print(df_train)
+
+def preprocess_image(filename):
+    """
+    Load the specified file as a JPEG image, preprocess it and
+    resize it to the target shape.
+    """
+
+    image_string = tf.io.read_file(filename)
+    image = tf.image.decode_jpeg(image_string, channels=3)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.image.resize(image, target_shape)
+    return image
 
